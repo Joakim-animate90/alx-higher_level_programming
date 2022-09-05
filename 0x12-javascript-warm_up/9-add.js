1ea81cf81aa1820d3e0cdfb97feec433a5cc3635
Joakim-animate90 <joakimbwire23@gmail.com>
2022-09-05 14:32:02 +0000

    [correction]:Task 10

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,4 +1,7 @@
 #!/usr/bin/node
-exports.add = function (a, b) {
-  return (a + b);
-};
+function add (a, b) {
+  const c = a + b;
+  console.log(c);
+}
+
+add(Number(process.argv[2]), Number(process.argv[3]));
